df4e47ff6cc0d6478e89bdcdbcebf2abb6c06bb8
zihuaiyou <976497972@qq.com>
2022-05-24 18:46:23 +0800

    handle css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import count from "./js/count";
 import sum from "./js/sum";
+import "./css/style.css"
 
 console.log(count(8,1));
 console.log(sum(1,2,3));
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 const path = require('path')
 module.exports = {
     // 入口文件
-    entry:"./src/main.js", //相对目录
+    entry: "./src/main.js", //相对目录
     // 输出
-    output:{
+    output: {
         // 输出文件目录
-        path:path.resolve(__dirname,"dist"), //绝对目录
+        path: path.resolve(__dirname, "dist"), //绝对目录
         // 输出文件名
-        filename:"main.js"
+        filename: "main.js"
     },
     // 配置loader
-    module:{
+    module: {
         // 配置规则
-        rules:[]
+        rules: [
+            {
+                test: /\.css$/,  //检测以css结尾的文件(正则)
+                use: [ //使用loader, 需要按顺序（从下往上执行）
+                    'style-loader', //将js中的css以style标签的形式添加到html中
+                    'css-loader' //将css资源编译呈commonjs模块形式添加到js中
+                ]
+            }
+        ]
     },
     // 插件
-    plugins:[],
+    plugins: [],
     // 模式
-    mode:"development"
+    mode: "development"
 }
