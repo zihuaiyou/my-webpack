82e8d3dca149f4a596b70a84a35427bf8d71620b
zihuaiyou <976497972@qq.com>
2022-06-03 22:56:03 +0800

    on-demand loading

--- CODE_SPLIT/src/main.js
+++ CODE_SPLIT/src/main.js
@@ -1,3 +1,15 @@
-import {sum} from './math'
-console.log(sum(1,2,3));
+import { sum } from './math'
+// import count from './count'
+console.log(sum(1, 2, 3));
 console.log('main');
+
+const btn = document.body.querySelector(".btn");
+btn.addEventListener("click", () => {
+    /**
+     * 实现按需加载
+     */
+    import('./count') //import函数动态导入，将导入的文件分割成模块，然后自动加载
+        .then(res => console.log(res,res.default(3,2)))
+        .catch(err => console.log(err))
+})
+
